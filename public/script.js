afb07b54bc3d8b4465b4fe5f4a91ac875ecf69fd
Desiree Mendes <desireefrost@live.ca>
2022-02-07 16:08:30 -0500

    refactor code

--- public/script.js
+++ public/script.js
@@ -7,12 +7,16 @@ function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return;
-
-  key.classList.add('playing');
   audio.currentTime = 0;
+  key.classList.add('playing');
   audio.play();
 }
 
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+window.addEventListener("load", (event) => {
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((key) =>
+    key.addEventListener("transitionend", removeTransition)
+  );
+
+  window.addEventListener("keydown", playSound);
+});
