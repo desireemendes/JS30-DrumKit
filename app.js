d4648870d4666eb232d55fd0f74b0e4a88348036
Desiree Mendes <desireefrost@live.ca>
2022-02-07 00:03:23 -0500

    Update port for heroku

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const port = 3000
 
 app.use(express.static("public"));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App listening on ${PORT}...`);
+});
